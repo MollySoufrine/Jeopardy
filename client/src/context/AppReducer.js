d9cc1c9293b29eaac4f5b8b8cd06025c22019b77
molly soufrine <mollysoufrine@gmail.com>
2020-11-25 17:00:39 -0500

    added two actions to reducer and created a few dispatches in globalstate

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,1 +1,14 @@
-export default
+export default (state, action) => {
+  switch (action.type) {
+    case "NEW_PLAYER":
+      return {
+        ...state,
+        playerName: [action.payload, ...state.playerName],
+      };
+    case "NEW_SCORE":
+      return {
+        ...state,
+        score: [action.payload, ...state.score],
+      };
+  }
+};
--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -2,20 +2,50 @@ import React, { createContext, useState } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
+  questions: [],
   score: [],
   playerName: [],
   time: 15,
 };
 
 export const GameContext = createContext(initialState);
-const GameContextProvider = (props) => {
+const GameContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+  function updatePlayerName(id) {
+    dispatch({
+      type: "NEW_PLAYER",
+      payload: id,
+    });
+  }
+  function updateScore(score) {
+    dispatch({
+      type: "NEW_SCORE",
+      payload: score,
+    });
+  }
+  function updateTime(timer) {
+    dispatch({
+      type: "UPDATE_TIME",
+      payload: timer,
+    });
+  }
+  function updateQuestionModal(questionID) {
+    dispatch({
+      type: "QUESTION_ANSWERED",
+      payload: questionID,
+    });
+  }
+
+  return (
+    <GameContextProvider
+      value={{
+        score: state.score,
+        playerName: state.playerName,
+        time: state.time,
+        questions: state.questions,
+      }}
+    >
+      {children}
+    </GameContextProvider>
+  );
 };
-
-const [showModal, setShowModal] = useState(true);
-
-const playerName = props.location.playerName;
-const [score, setScore] = useState(0);
-
-const [questionCount, setCount] = useState(0);
-const history = useHistory();
