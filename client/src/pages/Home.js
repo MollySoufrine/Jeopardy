2da86aa138f4117f9e25cec92f29ef1a2e464522
molly soufrine <mollysoufrine@gmail.com>
2021-01-25 17:27:05 -0500

    configure useReducer to add player...this way it could be a multiplayer game

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,24 @@
-import React, { useReducer } from "react";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+import React, { useReducer, useState } from "react";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_NEW_PLAYER":
+      return {
+        ...state,
+        players: [...state.players, { player: action.player }],
+        //return players already there and the new one
+      };
+    default:
+      return state;
+  }
+};
+
+// const addNewPlayer = (player) =>
+//   dispatch({ type: "ADD_NEW_PLAYER", players: [player] });
 export const Home = () => {
+  //state value and function called dispatch
+  const [{ players }, dispatch] = useReducer(reducer, { players: [] });
+  const [player, setPlayer] = useState();
   return (
     <div className="readyBanner">
       <p className="intro-tag">Are you Ready to Play Jeopardy?</p>
@@ -15,11 +30,17 @@ export const Home = () => {
       />
 
       <div className="player_names_input">
-        <InputGroup className="mb-3">
-          <FormControl />
-
-          <Button>Add New Player</Button>
-        </InputGroup>
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch({ type: "ADD_NEW_PLAYER", player });
+          }}
+        >
+          <input value={player} onChange={(e) => setPlayer(e.target.value)} />
+          <pre>{JSON.stringify(players, null, 2)}</pre>
+          <button>Add New Player:</button>
+        </form>
       </div>
     </div>
   );
