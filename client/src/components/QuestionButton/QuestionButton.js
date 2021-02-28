4ae43054d6b15389568ec8716cf56e2150eb1237
molly soufrine <mollysoufrine@gmail.com>
2021-02-28 17:43:19 -0500

    refractor

--- client/src/components/QuestionButton/QuestionButton.js
+++ client/src/components/QuestionButton/QuestionButton.js
@@ -1,27 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { GameContext } from "../../App";
 
-function QuestionButton({ question, onQuestionChange, disabled }) {
+function QuestionButton({ question, onQuestionChange }) {
   const { state } = useContext(GameContext);
 
   const handleClick = () => {
-    if (!state.disabledQuestions.includes(question.id)) {
-      console.log(question.id);
-      //if question is not disabled, show question
-      onQuestionChange(question.id);
-    } else {
-      alert("this question is disabled");
-    }
+    debugger;
+
+    onQuestionChange(question.id);
   };
 
   return (
-    <Button disabled={disabled} className="game-button" onClick={handleClick}>
-      {/* {console.log(question)}
-      {console.log(choices)} */}
-      {/* make a component for each catrgory, filter list of questions down
-            {/* make a component for each catrgory, filter list of questions down
-              to specific category, print title in first row */}
+    <Button
+      disabled={state.disabledQuestions.includes(question.id)}
+      className="game-button"
+      onClick={handleClick}
+    >
       {question.score}
     </Button>
   );
